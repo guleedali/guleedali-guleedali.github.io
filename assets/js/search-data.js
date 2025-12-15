11886736da42d7e2022a336e0285cc1e5ec39e55
guleedali <167659755+guleedali@users.noreply.github.com>
2025-12-15 21:03:23 +0000

    Deploying to gh-pages from @ guleedali/guleedali.github.io@2783fc87c6d9bf846bc7d458ad997987a9e80b95 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "LAYERS frontiers.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
